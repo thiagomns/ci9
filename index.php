2feee556905c95c28084764913187a49a0019974
Thiago de Melo Nunes dos Santos <thiagomns@hotmail.com>
2013-04-02 00:40:31 -0300

    1 atualização

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" media="all" href='style.css' /> 
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<div class="container">
+    	<div class="sombraEsquerda">E</div>
+           	<div class="site">
+                <div class="header">
+                	<div class="barraPesquisa"><img src="imgs/barrapesquisa_03.jpg" /></div>
+                    <div class="logoheader"><img src="imgs/logoheader_03.jpg"/></div>
+                </div><!-- header -->
+            	<div class="content">
+            		<div class="slide"></div>
+        			  	
+                	<div class="colunaEsquerda">
+                    	<div class="cabeçalho">COrrimãos</div>
+                        
+                        <img src="imgs/colunaesquerda.jpg"><br>
+                        "Quando é verdadeiro, o tempo não enfraquece e ninguém substitui." — com Amanda Niemeyer.
+                    </div>
+              		<div class="colunaDireita"><div class="cabeçalho">colD</div>
+                    <img src="imgs/colunadireita.jpg">
+                    "Quando é verdadeiro, o tempo não enfraquece e ninguém substitui." — com Amanda Niemeyer.
+                    </div>
+              		<div class="colunaMeio"><div class="cabeçalho">colM</div>
+                    <img src="imgs/colunaMeio.jpg">
+                    "Quando é verdadeiro, o tempo não enfraquece e ninguém substitui." — com Amanda Niemeyer.
+                    </div>
+                    <div class="clearfix"></div>
+                   
+              	</div><!-- content -->
+            	<div class="footer">
+            		<div class="barraCinza">Cinza</div>
+                	<div class="barraBranca">Branco</div>
+            	</div><!-- footer -->
+        	</div><!-- site -->
+        <div class="sombraDireita">D</div>
+    </div>        
+</body>
+</html>
